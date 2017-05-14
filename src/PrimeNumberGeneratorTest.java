c6f4962757afed5b32207ca3f5960619bfe0af1e
willi314 <willi314@muohio.edu>
2017-05-13 20:57:15 -0400

    Added set up for tests

--- src/PrimeNumberGeneratorTest.java
+++ src/PrimeNumberGeneratorTest.java
@@ -1,18 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrimeNumberGeneratorTest {
 
+	PrimeNumberGenerator generator;
+	
+	@Before
+	public void init(){
+		generator = new PrimeNumberGenerator();
+	}
+	
 	@Test
 	public void TwoIsAPrimeNumber() {
-		PrimeNumberGenerator generator = new PrimeNumberGenerator();
 		assertEquals(true, generator.isPrime(2));
 	}
 	
 	@Test
 	public void OneIsNotAPrimeNumber(){
-		PrimeNumberGenerator generator = new PrimeNumberGenerator();
 		assertEquals(false, generator.isPrime(1));
 	}
 
