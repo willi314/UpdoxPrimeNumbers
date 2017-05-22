bb1153a801566fb7d198600663df3625dcf5ed67
willi314 <willi314@muohio.edu>
2017-05-21 22:25:57 -0400

    Added some input checking so only integers are allowed

--- src/PrimeNumberGeneratorApp.java
+++ src/PrimeNumberGeneratorApp.java
@@ -8,10 +8,18 @@ public class PrimeNumberGeneratorApp {
 		
 		System.out.println("Enter -1 at any time to exit.");
 		while(true){
-			System.out.print("Enter Range Start: ");
+			System.out.print("Enter range start: ");
+			while (!consoleInput.hasNextInt()) {
+				   System.out.println("Please enter a valid integer");
+				   consoleInput.nextLine();
+			}
 			int startingValue = consoleInput.nextInt();
 			if(startingValue == -1) break; 
-			System.out.print("Enter Range End: ");
+			System.out.print("Enter range end: ");
+			while (!consoleInput.hasNextInt()) {
+				   System.out.println("Please enter a valid integer");
+				   consoleInput.nextLine();
+			}
 			int endingValue = consoleInput.nextInt();
 			if(endingValue == -1) break;		
 			System.out.println("Prime numbers between " + startingValue + " and " + endingValue + ": " + generator.generate(startingValue, endingValue).toString());
